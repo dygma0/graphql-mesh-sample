32e72f7d4577f569c014c4eb7da45069b9b36dee
Byeongju, Shin <404err@naver.com>
2024-04-28 10:47:27 +0900

    feat: Swagger API 설정 추가

--- apps/notification-service/src/main.ts
+++ apps/notification-service/src/main.ts
@@ -3,20 +3,48 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
+import { INestApplication, Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+function setUpSwagger(app: INestApplication) {
+  const options = new DocumentBuilder()
+    .setTitle('Notification Service')
+    .setDescription('The Notification Service API description')
+    .setVersion('1.0')
+    .addTag('notification')
+    .build();
+  SwaggerModule.setup(
+    'api-docs',
+    app,
+    SwaggerModule.createDocument(app, options)
+  );
+}
+
+function runServer(app: INestApplication, globalPrefix: string) {
+  const port = process.env.PORT || 3003;
+  app
+    .listen(port)
+    .then(() => {
+      Logger.log(
+        `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+      );
+    })
+    .catch((error) => {
+      Logger.error('💥 Failed to start the application', error);
+    });
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
+
+  const globalPrefix = 'notification-service';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
+
+  setUpSwagger(app);
+  runServer(app, globalPrefix);
 }
 
 bootstrap();
